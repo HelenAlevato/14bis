7e2a308ca8d8fb463f03ea8efccb2f6dc2f2cf2e
Flavio Alessandro Pereira <flavioapereira@yahoo.com.br>
2021-04-18 12:02:49 -0300

    Feature/implementação do controller exception

--- 14BisServer/src/main/java/com/fingerone/BisServer/controller/CodeListController.java
+++ 14BisServer/src/main/java/com/fingerone/BisServer/controller/CodeListController.java
@@ -1,23 +1,66 @@
 package com.fingerone.BisServer.controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fingerone.BisServer.helper.ExcelHelper;
+import com.fingerone.BisServer.message.ResponseMessage;
+import com.fingerone.BisServer.model.CodeList;
+import com.fingerone.BisServer.service.ExcelService;
 
-@CrossOrigin("http://localhost:3000")
+
+@CrossOrigin("http://localhost:8081")
 @Controller
-@RequestMapping("/api/codelist")
+@RequestMapping("/api/excel")
 public class CodeListController {
 
+	@Autowired
+	ExcelService fileService;
+
 	@PostMapping("/upload")
-	public void uploadFile(@RequestParam("file") MultipartFile file) {
+	public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+		String message = "";
 
-		
+		if (ExcelHelper.hasExcelFormat(file)) {
+			try {
+				fileService.save(file);
 
+				message = "Uploaded the file successfully: " + file.getOriginalFilename();
+				return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+			} catch (Exception e) {
+				message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+				return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+			}
+		}
+
+		message = "Please upload an excel file!";
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
 	}
+
+	@GetMapping("/codelists")
+	public ResponseEntity<List<CodeList>> getAllTutorials() {
+		try {
+			List<CodeList> tutorials = fileService.getAllCodelists();
+
+			if (tutorials.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+
+			return new ResponseEntity<>(tutorials, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
 }
