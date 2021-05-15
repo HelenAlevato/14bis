238d7725a6386a55887e32e2dfaa7e49d09d0c87
HelenAlevato <helen-alevato@hotmail.com>
2021-05-15 20:16:35 -0300

    Feature/ Adição da Aba "LEP" na página de Codelist

--- 14bis-ui/src/content/CodeListPage/CodeListPage.js
+++ 14bis-ui/src/content/CodeListPage/CodeListPage.js
@@ -1,19 +1,19 @@
 import { Button, Tabs, Tab } from 'carbon-components-react';
 import React, { useEffect, useState } from 'react';
 import {
-    DataTable,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableToolbar,
-    TableToolbarContent
-  } from 'carbon-components-react';
-import { TableSplit24 } from '@carbon/icons-react';
-  
+  DataTable,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableToolbar,
+  TableToolbarContent
+} from 'carbon-components-react';
+import { Book24, Document24, DocumentView24, TableSplit24 } from '@carbon/icons-react';
+
 const headers = [
   {
     key: 'secao',
@@ -45,10 +45,35 @@ const headers = [
   },
 ];
 
+const headersTableLEP = [
+  {
+    key: 'full',
+    header: 'Full',
+  },
+  {
+    key: 'delta',
+    header: 'Delta',
+  },
+  {
+    key: 'lep',
+    header: 'LEP',
+  },
+  {
+    key: 'remarks',
+    header: 'Remarks',
+  },
+  {
+    key: 'name',
+    header: 'Name',
+  },
+];
+
+const doctTypes = ["Mars","Alpha Centauro","Saturno","Jupiter","Moon","Pluto"]
+
 const CodeListPage = () => {
   var [rows, setRows] = useState([]);
   var [isLoaded, setLoaded] = useState(false);
-  
+
   useEffect(async () => {
     if (!isLoaded) {
       setRows(await fetch("http://localhost:8585/api/codelist").then(response => response.json()))
@@ -57,57 +82,117 @@ const CodeListPage = () => {
   })
 
   return (
-    
-    <TableContainer title="Nome do manual" description="Tabela do Codelist">
-    <Tabs type="container">
-      <Tab id="Tab1" label="Codelist"> 
-          <DataTable rows={rows} headers={headers}>
-            {({
-              rows,
-              headers,
-              getHeaderProps,
-              getRowProps,
-              getTableProps,
-            }) => (
-            
-            <Table>
-            <TableToolbar>
-              <TableToolbarContent>              
-                <Button onClick={() => {}} renderIcon={TableSplit24}>Codelist </Button>
-              </TableToolbarContent>
-            </TableToolbar>
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map(header => (
-                    <TableHeader
-                      key={header.key}
-                      {...getHeaderProps({ header })}>
-                      {header.header}
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map(row => (
-                  <TableRow key={row.id} {...getRowProps({ row })}>
-                    {row.cells.map(cell => (
-                      <TableCell key={cell.id}>{cell.value}</TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-            </Table>
-        )}
-      </DataTable>
+    <>
+      <TableContainer title="Nome do manual" description="Tabela do Codelist">
+        <Tabs type="container">
+          <Tab id="Tab1" label="Codelist">
+            <DataTable rows={rows} headers={headers}>
+              {({
+                rows,
+                headers,
+                getHeaderProps,
+                getRowProps,
+                getTableProps,
+              }) => (
 
-      </Tab>
-      <Tab id="Tab2" label="LEP">
+                <Table>
+                  <TableToolbar>
+                    <TableToolbarContent>
+                      <Button onClick={() => { }} renderIcon={TableSplit24}>Codelist </Button>
+                    </TableToolbarContent>
+                  </TableToolbar>
+                  <Table {...getTableProps()}>
+                    <TableHead>
+                      <TableRow>
+                        {headers.map(header => (
+                          <TableHeader
+                            key={header.key}
+                            {...getHeaderProps({ header })}>
+                            {header.header}
+                          </TableHeader>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map(row => (
+                        <TableRow key={row.id} {...getRowProps({ row })}>
+                          {row.cells.map(cell => (
+                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                          ))}
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </Table>
+              )}
+            </DataTable>
 
-      </Tab>
-    </Tabs>
-    </TableContainer>
+          </Tab>
+
+          <Tab id="Tab2" label="LEP">
+            <DataTable rows={rows} headers={headersTableLEP}>
+              {({
+                rows,
+                headers,
+                getHeaderProps,
+                getRowProps,
+                getTableProps,
+              }) => (
+
+                <Table>
+                  <TableToolbar>
+                    <TableToolbarContent>
+                      <Button onClick={() => { }} renderIcon={TableSplit24}>Codelist </Button>
+                    </TableToolbarContent>
+                  </TableToolbar>
+                  <Table {...getTableProps()}>
+                    <TableHead>
+                      <TableRow>
+                        {headers.map(header => (
+                          <TableHeader
+                            key={header.key}
+                            {...getHeaderProps({ header })}>
+                            {header.header}
+                          </TableHeader>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map(row => (
+                        <TableRow key={row.id} {...getRowProps({ row })}>
+                          <TableCell key={"full"}>
+                            <Book24 />
+                          </TableCell>
+                          <TableCell key={"delta"}>
+                            <Document24 />  
+                          </TableCell>
+                          <TableCell key={"lep"}>
+                            <DocumentView24 />
+                          </TableCell>
+                          <TableCell key={"remarks"}>
+                            {"-50"}
+                          </TableCell>
+                          <TableCell key={"name"}>
+                            {doctTypes[Math.floor((Math.random() * 6))]}
+                          </TableCell>
+                          {/* {row.cells.map(cell => (
+                            <TableCell key={cell.id}>{cell.value + " | segunda aba"}</TableCell>
+                          ))} */}
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </Table>
+              )}
+            </DataTable>
+          </Tab>
+        </Tabs>
+      </TableContainer>
+      
+      <div>
+        <Button>Fechar</Button>
+      </div>
+    </>
   );
 };
 
