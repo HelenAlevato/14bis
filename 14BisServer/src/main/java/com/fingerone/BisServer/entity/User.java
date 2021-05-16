3ac1c0164f2f09d57d491f9e5195adb846de4565
rangelandrade <jrangel.andrade@gmail.com>
2021-05-16 12:46:50 -0300

    Feature/adição da entidade usuário

---
+++ 14BisServer/src/main/java/com/fingerone/BisServer/entity/User.java
@@ -0,0 +1,21 @@
+package com.fingerone.BisServer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "fatec_user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String cpf;
+}
