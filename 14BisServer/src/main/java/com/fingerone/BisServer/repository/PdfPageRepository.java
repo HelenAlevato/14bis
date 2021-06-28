28700a0984df78382b6482b740753ed90f191822
HelenAlevato <helen-alevato@hotmail.com>
2021-06-27 23:23:18 -0300

    busca de todas as páginas pelo bloco e pelo codigo

--- 14BisServer/src/main/java/com/fingerone/BisServer/repository/PdfPageRepository.java
+++ 14BisServer/src/main/java/com/fingerone/BisServer/repository/PdfPageRepository.java
@@ -1,11 +1,15 @@
 package com.fingerone.BisServer.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.fingerone.BisServer.entity.CodeList;
 import com.fingerone.BisServer.entity.PdfPage;
 
 @Repository
 public interface PdfPageRepository extends CrudRepository<PdfPage, Long> {
     PdfPage findFirstByBlockAndCodeAndPage(String block, String code, Long page);
+    Collection<PdfPage> findByBlockAndCode(String block, String code);
 }
